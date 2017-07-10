973d7e510aa99cb2d38a98b9b3ee8c5c70a2ecb3
Orcke <orcke.vn@gmail.com>
2017-07-10 08:38:31 +0700

    change time to play
    change time to play

--- Assets/Scripts/GameInfo.cs
+++ Assets/Scripts/GameInfo.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 
 public class GameInfo {
-	public float timeRemainingLevel =10;
+	public float timeRemainingLevel = 90;
 	public int level = 1;
 	public float velocityLevel = 0.015F;
 
